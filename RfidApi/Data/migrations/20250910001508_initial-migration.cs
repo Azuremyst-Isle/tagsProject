4839e4324e3887abbb63406d6e2ff835e17dbe0b
Antonio Herrera Martin <44150484+alfa33333@users.noreply.github.com>
2025-09-09 20:26:53 -0600

    Add unique index to rfid_tag in Item model and update migration files

--- RfidApi/Data/migrations/20250910001508_initial-migration.cs
+++ RfidApi/Data/migrations/20250910001508_initial-migration.cs
@@ -15,7 +15,8 @@ namespace RfidApi.data.migrations
                 name: "item",
                 columns: table => new
                 {
-                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                    Id = table
+                        .Column<int>(type: "INTEGER", nullable: false)
                         .Annotation("Sqlite:Autoincrement", true),
                     rfid_tag = table.Column<string>(type: "TEXT", nullable: false),
                     name = table.Column<string>(type: "TEXT", nullable: false),
@@ -23,19 +24,19 @@ namespace RfidApi.data.migrations
                     status = table.Column<string>(type: "TEXT", nullable: false),
                     certification_code = table.Column<string>(type: "TEXT", nullable: true),
                     owner_name = table.Column<string>(type: "TEXT", nullable: true),
-                    last_updated = table.Column<DateTime>(type: "TEXT", nullable: false)
+                    last_updated = table.Column<DateTime>(type: "TEXT", nullable: false),
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_item", x => x.Id);
-                });
+                }
+            );
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "item");
+            migrationBuilder.DropTable(name: "item");
         }
     }
 }
--- RfidApi/Models/Item.cs
+++ RfidApi/Models/Item.cs
@@ -1,14 +1,15 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace RfidApi.Models;
 
+[Index(nameof(rfid_tag), IsUnique = true)]
 public class Item
 {
     public int Id { get; set; }
 
     public required string rfid_tag { get; set; }
-
     public required string name { get; set; }
     public string? description { get; set; }
     public string status { get; set; } = "available";
